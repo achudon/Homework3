278eecec90b4c8b72df3726459e01500f6786891
Alyssa <ahudon@uw.edu>
2016-04-05 17:22:00 -0700

    added basic calculator and array

--- Homework3/Array.swift
+++ Homework3/Array.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+// takes an array of Ints as a parameter, returns sum of all Ints in array
+func add(numArray: [Int]) -> Int {
+    var result = 0
+    
+    for index in 0...numArray.count {
+        result += numArray[index]
+    }
+    
+    return result
+}
+
+// takes an array of Ints as a parameter, returns product of all Ints in array
+
+func multiply(numArray: [Int]) -> Int {
+    var result = 0
+    
+    for index in 0...numArray.count {
+        result *= numArray[index]
+    }
+    return result
+}
+
+// takes an array of Ints as a parameter, returns count of all Ints in array
+
+func count(numArray: [Int]) -> Int {
+    return numArray.count
+}
+
+// takes an array of Ints as a parameter, returns aerage of all elements in array
+
+func average(numArray: [Int]) -> Int {
+    let arraySum = add(numArray)
+    let arrayCount = count(numArray)
+    
+    return arraySum / arrayCount
+}
+
+// take in an operand and an array of Ints as parameters, returns the result of that operation
+
+func genericCalc(operand: String, numArray: [Int]) -> Int{
+    switch operand {
+        case "+":
+            return add(numArray)
+        case "*":
+            return multiply(numArray)
+        case "count":
+            return count(numArray)
+        case "avg":
+            return average(numArray)
+        default:
+            print("Please enter valid values for an operation")
+    }
+    return 0
+}
+
--- Homework3/Calculator.swift
+++ Homework3/Calculator.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+// takes two Ints as parameters, returns the Int sum of the two numbers
+func add(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+
+// takes two Ints as parameters, returns an Int, the result of (first number) - (second number)
+func subtract(num1: Int, num2: Int) -> Int {
+    return num1 - num2
+}
+
+// takes two Ints as parameters, returns the Int product of the two numbers
+func multiply(num1: Int, num2: Int) -> Int {
+    return num1 * num2
+}
+
+// takes two Ints as parameters, returns the Int quotient of the two numbers
+func divide(num1: Int, num2: Int) -> Int {
+    return num1 / num2
+}
+
+// take in an operand and two Ints as parameters, returns the result of that operation
+func genericCalc(operand: String, num1: Int, num2: Int) -> Int {
+    switch operand {
+    case "+":
+        return add(num1, num2: num2)
+    case "-":
+        return subtract(num1, num2: num2)
+    case "*":
+        return multiply(num1, num2: num2)
+    case "/":
+        return divide(num1, num2: num2)
+    default:
+        print("Can't complete operation with given values.")
+    }
+    return 0
+}
